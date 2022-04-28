a8543ce736d2cd6a8e1292489c88df028b6aa1ee
Soojin Hwang <soojiny.hwang@gmail.com>
2022-04-28 20:31:43 +0100

    index, new and create functions and views completed

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,7 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 20}
+  validates :price_per_night, presence: true, numericality: { only_integer: true }
+  validates :number_of_guests, presence: true, numericality: { only_integer: true }
 end
